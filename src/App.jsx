b1c09871bd39e87ed9f201a96b5c5cabbe5d15dc
Anamul029 <anamhaque.5101@gmail.com>
2024-10-26 13:57:18 +0600

    final commit for finish project

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <div className="bg-[#2c2929] w-[500px] md:w-full font-immortal min-h-screen text-white"> 
         <NavBar />
-        <div className="flex flex-col bg-blue-950 mt-1  md:flex-row">
+        <div className="flex flex-col bg-blue-950 mt-1 min-h-screen  md:flex-row">
           <Left_Side />
           <Right_Side />
           {/* ab */}
